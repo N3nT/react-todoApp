b8c47aa326770424004c966cfe256ffc28bc3ac9
N3nT <bar55v71@interia.pl>
2025-01-06 12:04:07 +0100

    Change design to look like whiteboard with sticky notes

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -25,21 +25,24 @@ export default function ToDo() {
 
     return(
         <>
-            <div className="todo-wrap">
-                <div className="todo-header">
-                    <h1 className="todo-headerText">ToDoApp by <a href="#">N3nT</a></h1>
+            <div className="todo-header">
+                <h1 className="todo-headerText">ToDoApp by <a href="#">N3nT</a></h1>
+                <div className="todo-addTask">
+                    <input type="text" placeholder='Task name' className='todo-addTaskInput' onChange={handleChangeInput} value={inputValue}/>
+                    <button className="todo-addTaskButton" onClick={handleAddTask}>Add</button>
                 </div>
+            </div>
+            <div className="todo-wrap">
                 <div className="todo-main">
-                    <div className="todo-addTask">
-                        <input type="text" placeholder='Task name' className='todo-addTaskInput' onChange={handleChangeInput} value={inputValue}/>
-                        <button className="todo-addTaskButton" onClick={handleAddTask}>Add</button>
-                    </div>
+                    
                     <div className="todo-tasksWrap">
                         {tasks.map((task, index) => 
                             <div className={clickedIndex.includes(index) ? "todo-task todo-taskComplete" :"todo-task" } key={index}>
                                 <span className="todo-taskName">{task}</span>
-                                <button className="todo-completeButton" onClick={() => handleCompleteTask(index)}>Complete</button>
-                                <button className="todo-deleteButton" onClick={() => handleDeleteTask(index)}>Delete</button>
+                                <div className="todo-taskControls">
+                                    <button className="todo-completeButton" onClick={() => handleCompleteTask(index)}>👍</button>
+                                    <button className="todo-deleteButton" onClick={() => handleDeleteTask(index)}>👎</button>
+                                </div>
                             </div>
                             )}
                     </div>
